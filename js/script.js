9662b55934e6bab9d0fdefd6b7999090dd9a97f9
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-05-29 18:39:54 +0200

    soluzione temporanea inserendo .value

--- js/script.js
+++ js/script.js
@@ -18,22 +18,22 @@ button.addEventListener('click', function(){
     console.log('età dichiarata ' + userAge.value);
 
     //validazione (controllo che i valori inseriti siano solo numeri maggiori di 0)
-    if(isNaN(userDistance) || isNaN(userAge) || userDistance < 1 || userAge < 1){
+    if(isNaN(userDistance.value) || isNaN(userAge.value) || (userDistance.value) < 1 || (userAge.value) < 1){
         console.log('valori non validi')
         document.getElementById('tiket-price').innerText = ('ERRORE! ricaricare la pagina e inserire numeri maggiori di 0')
     } else{
 
         //calcola prezzo biglietto basandosi sui km scelti dell'utente tenendo in considerazione il costo di 0.21€ al km
-        const basePrice = userDistance * 0.21;
+        const basePrice = (userDistance.value) * 0.21;
         console.log('prezzo del biglietto base ' + basePrice);
 
         let reducedPrice;
 
         //applicazione sconti
-        if (userAge < 18) {
+        if ((userAge.value) < 18) {
             reducedPrice = (basePrice * 20) / 100;
             console.log('costo biglietto applicando sconto del 20% ' + (basePrice - reducedPrice) + '€');
-        } else if (userAge > 65) {
+        } else if ((userAge.value) > 65) {
             reducedPrice = (basePrice * 40) / 100;
             console.log('costo biglietto applicando sconto del 40% ' + (basePrice - reducedPrice) + '€');
         } else {
